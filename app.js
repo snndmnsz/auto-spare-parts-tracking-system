5deebf3f7aba72dfd9c615deba4a15db3a0a1042
snndmnsz <snndmnsz98@gmail.com>
2021-12-04 19:29:56 +0300

    menu pages added

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+
 const main = require('./routes/mainRoutes');
-
-
 const app = express();
 
 
@@ -15,14 +14,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 app.use(main);
-
-
-//app.use(errorController.get404);
-
-// db.sync()
-//   .then((result) => {
-//     app.listen(3000);
-//   })
-//   .catch((err) => console.log(err));
 
 app.listen(3000, () => console.log(`app listening on port 3000!`));
--- controllers/mainPage.js
+++ controllers/mainPage.js
@@ -1,54 +1,66 @@
 exports.getLoginPage = (req, res, next) => {
-    res.render('login.ejs');
+  res.render("login.ejs");
 };
 
 exports.postLoginPage = (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
+  console.log(req.body);
+  res.redirect("/");
 };
 
 exports.getMainPage = (req, res, next) => {
-    res.render('main.ejs');
+  res.render("main.ejs");
 };
 
 exports.getNewProduct = (req, res, next) => {
-    res.send('getNewProduct');
+  res.render("menuView.ejs",{
+    pageHeader:'Add New Product'
+  });
 };
 
 exports.getStock = (req, res, next) => {
-    res.send('getStock');
+    res.render("menuView.ejs",{
+        pageHeader:'Stocks'
+      });
 };
 
-
 exports.getEditProduct = (req, res, next) => {
-    res.send('getEditProduct');
+    res.render("menuView.ejs",{
+        pageHeader:'Edit Product'
+      });
 };
 
 exports.getNewOrder = (req, res, next) => {
-    res.send('getEditOrder');
+    res.render("menuView.ejs",{
+        pageHeader:'Create New Order'
+      });
 };
 
 exports.getActiveOrders = (req, res, next) => {
-    res.send('getActiveOrders');
+    res.render("menuView.ejs",{
+        pageHeader:'Active Orders'
+      });
 };
-
 
 exports.getEditOrder = (req, res, next) => {
-    res.send('getEditOrder');
+    res.render("menuView.ejs",{
+        pageHeader:'Edit Order'
+      });
 };
 
-
 exports.getRequestProduct = (req, res, next) => {
-    res.send('getRequestProduct');
+    res.render("menuView.ejs",{
+        pageHeader:'Request a Product'
+      });
 };
 
 exports.getPartSearch = (req, res, next) => {
-    res.send('getPartSearch');
+    res.render("menuView.ejs",{
+        pageHeader:'Part Search System'
+      });
 };
 
 exports.getCheckParts = (req, res, next) => {
-    res.send('getCheckParts');
+    res.render("menuView.ejs",{
+        pageHeader:'Check Part Compability'
+      });
 };
-
-
-
